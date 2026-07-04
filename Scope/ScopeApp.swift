aca94fc0ccff6f8ae360567ea842ec3f3ab421ee
Tobias J <67707510+tobias-jermain@users.noreply.github.com>
2026-07-04 07:51:25 +0100

    Initial Commit

---
+++ Scope/ScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScopeApp.swift
+//  Scope
+//
+//  Created by Tobias Jermain on 04/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
